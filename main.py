99a7f49f40fb1afd9749f3aab059d57d05875386
Omar El <uplinks@hotmail.com>
2018-01-09 20:21:33 -0500

    Create

--- main.py
+++ main.py
@@ -1,51 +1,11 @@
+from flask import Flask
 
-<!DOCTYPE html>
-<html>
-    <head>  
-        <style>
-            .error_info {
-                color: #00529B;
-                background-color: #BDE5F8;
-        </style>
-    </head>
-    <body>
-    <h1 style="color:blue;fond-family:Verdana">Please enter your information below</h>
-    <p style="color:red;font-family:courier">E-mail is optional</p>
-        <from method="post">
-            <table>
-                <tr>
-                    <td><label for="username">Enter your name</label></td>
-                    <td>
-                        <input name="username" type="text" value="">
-                        <span class="error_info">This box cannot be empty</span>
-                    </td>
-                </tr>
+app = Flask(__name__)
+app.config['DEBUG'] = True
 
-                <tr>
-                    <td><label for="password">Creat a password</label></td>
-                    <td>
-                        <input name="password" type="password">
-                        <span class="error_info">This box cannot be empty</span>
-                    </td>
-                </tr>
+@app.route("/")
+def index():
+    return "Hello World"
 
-                <tr>
-                    <td><label for="verify"> Re-type your password</label></td>
-                    <td>
-                        <input name="verify" type="password">
-                        <span class="error_info">password donnot match!</span> 
-                    </td>
-                </tr>
-
-                <tr>
-                    <td><label for="email" E-mail (Ootional)</label></td>
-                    <td>
-                        <input name="email" values="">
-                        <span class="error_info"></span>
-                    </td>
-                </tr>   
-            </table>
-            <input type="submit">
-        </form>
-    </body>
-</html>
+app.run()
+ 
